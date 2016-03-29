db0873c5c90f7a7ed05d74e7e0b75105b0731ac4
praj25 <sail@IAZI.LOCAL>
2016-03-29 09:18:29 +0530

    updated version 4

--- app/scripts/controllers/employeeEdit.js
+++ app/scripts/controllers/employeeEdit.js
@@ -2,6 +2,8 @@ var myApp = angular.module("employeeApp");
 myApp.controller('EmployeeEdit', ['$scope','$http','$location',function ($scope,$http,$location) {
 
 
+$scope.deletedCertifications = [];
+$scope.deletedQualifications = [];
 
 
 //-----------------------------To add qualification-------------------------------------//
@@ -37,6 +39,110 @@ myApp.controller('EmployeeEdit', ['$scope','$http','$location',function ($scope,
  						$scope.certifications.push({'certification_name': user.certification_selected.certification_name, 'year':user.year,'certification_code':user.certification_selected.certification_code,'add':true})		
 			}
 		}
+//-----------------------Select id for delete Certification-------------------//	
+
+			$scope.getDeleteId_Qualification = function(btn) {
+				     console.log(this)
+				$scope.deleteId = this.data.sr_no;
+				$scope.buttonClicked = btn;
+
+  			};
+
+//------------------Select id for delete Qualification-------------------//
+  			$scope.getDeleteId_Certification = function(btn) {
+        	$scope.deleteId = this.data.sr_no;
+        	$scope.buttonClicked = btn;
+  			};
+//----------------Select id of deleted Salary-----------------------------//
+  			$scope.getDeleteId_Salary = function(btn) {
+
+        	$scope.deleteId = $scope.salary[this.$index].sr_no;
+        	$scope.buttonClicked = btn;
+  			};
+
+//--------------------Delete certification from modal--------------------//
+			$scope.deleteCertification = function() {
+			    for (i = 0; i < $scope.certifications.length; i++) {
+			            if ($scope.certifications[i].sr_no == $scope.deleteId) {
+			               if ($scope.certifications[i].add!=true)
+			                	$scope.deletedCertifications.push($scope.certifications[i].sr_no);
+			                $scope.certifications.splice(i, 1);
+			            }
+			        }
+  			  };
+
+//--------------------Delete qualification from modal--------------------//
+  			$scope.deleteQualification = function() {
+			    for (i = 0; i < $scope.qualifications.length; i++) {
+			            if ($scope.qualifications[i].sr_no == $scope.deleteId) {
+			                if ($scope.qualifications[i].add!=true)
+			                	$scope.deletedQualifications.push($scope.qualifications[i].sr_no);
+			                $scope.qualifications.splice(i, 1);
+			            }
+			        }
+			};
+//-----------------Delete Salary------------------------------------//
+			$scope.deleteSalary = function(){
+				for(i=0;i<$scope.salary.length;i++)
+				{
+					if($scope.salary[i].sr_no == $scope.deleteId){
+			                $scope.salary.splice(i, 1);
+					}
+				}
+			}
+//---------------To update the changes------------------------------//
+			$scope.saveData = function(emp)
+			{
+
+				for(i=0;i<$scope.deletedCertifications.length;i++)
+				{
+					console.log("data: "+$scope.deletedCertifications[i])
+					$http.delete("http://localhost:1337/employee/deleteCertification?id="+$scope.deletedCertifications[i]).success(function(result){
+
+						console.log(result);
+					})
+				}
+				for(i=0;i<$scope.deletedQualifications.length;i++)
+				{
+					$http.delete("http://localhost:1337/employee/deleteQualification?id="+$scope.deletedQualifications[i]).success(function(result){
+						console.log(result);
+					})
+				}	
+
+				for(i=0;i<$scope.qualifications.length;i++)
+				{
+					if($scope.qualifications[i].add==true)
+					{
+
+						var data = ({
+							emp_id:emp.emp_id,
+							qualification_code:$scope.qualifications[i].qualification_code,
+					    	percentage:$scope.qualifications[i].percentage
+						})
+						$http.post("http://localhost:1337/employee/insertQualification",data).success(function(result){
+							console.log(result)
+						})
+					}
+
+				}
+				console.log("certification")
+				console.log($scope.certifications)
+				for(i=0;i<$scope.certifications.length;i++)
+				{
+
+					if($scope.certifications[i].add==true)
+					{
+						var data =({
+							emp_id:emp.emp_id,
+							year:$scope.certifications[i].year,
+							certification_code:$scope.certifications[i].certification_code
+						})
+						$http.post("http://localhost:1337/employee/insertCertification",data)
+					}
+												
+				}
+
+			}
 
 
 
